586d483f86cbf30efafb1fa237ab61a182aba8bb
Ryan Nemiroff <ryguyn@gmail.com>
2020-12-11 19:15:16 -0800

    Add quad x with outputs scaled by 0.25

--- src/lib/mixer/MultirotorMixer/geometries/tools/px_generate_mixers.py
+++ src/lib/mixer/MultirotorMixer/geometries/tools/px_generate_mixers.py
@@ -344,6 +344,9 @@ if __name__ == '__main__':
         # Normalize mixer
         B_px = normalize_mix_px4(B)
 
+        if geometry['info']['key'] == '4xlt':
+            B_px *= 0.25 # Scale motor outputs by 1/4
+
         # Store matrices in geometry
         geometry['mix'] = {'A': A, 'B': B, 'B_px': B_px}
 
